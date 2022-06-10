7a0817c9dbc6138db7e1450eed3704e95368d92f
Radiant <ronniezhu2017@gmail.com>
2022-06-10 14:40:42 -0700

    initialize project_2 with navbar, movieinfo

--- montage/src/App.js
+++ montage/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import MovieInfo from "./components/MovieInfo";
+import CoverPage from "./components/CoverPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <MovieInfo />
+      <CoverPage />
     </div>
   );
 }
--- montage/src/components/MovieInfo.js
+++ montage/src/components/MovieInfo.js
@@ -3,10 +3,20 @@ rating, director, writer, genre, and description */
 export default function MovieInfo() {
     return (
         <div class = "MovieInfo">
-            <div id = "MovieInfoContainer">
-                <p class = "MovieInfoTitle">Title</p>
-                <p class = "MovieInfoYear">1999</p>
+            <div class = "MovieInfoContainer">
+                <div class="TitleYearContainer">
+                    <p class = "MovieInfoTitle">となりのトトロ</p>
+                    <p class = "MovieInfoYear">1988</p>
+                </div>
+                <img class = "MovieInfoPoster" src={ require('../images/poster.png') } alt="poster"/>
+                <div class = "GeneralInfoContainer">
+                    <p class = "MovieInfoDirector">Director: Hayao Miyazaki</p>
+                    <p class = "MovieInfoWriter">Writer: Hayao Miyazaki</p>
+                    <p class = "MovieInfoGenre">Genre: Animation/Comedy/Family</p>
+                </div>
 
+                <p class = "MovieInfoRating">10 / 10 ★</p>
+                <p class = "MovieInfoDescription">Movie Description: The adventure story of two girls and forest spirits</p>
             </div>
         </div>
     )
