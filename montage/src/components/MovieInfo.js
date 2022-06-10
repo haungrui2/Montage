ef59eee4f8ad860483c866f73368e15bfb9cc566
Radiant <ronniezhu2017@gmail.com>
2022-06-09 17:47:28 -0700

    add library

--- montage/src/components/MovieInfo.js
+++ montage/src/components/MovieInfo.js
@@ -1,2 +1,13 @@
 /* This is the page for movie information, including movie name, the poster,
 rating, director, writer, genre, and description */
+export default function MovieInfo() {
+    return (
+        <div class = "MovieInfo">
+            <div id = "MovieInfoContainer">
+                <p class = "MovieInfoTitle">Title</p>
+                <p class = "MovieInfoYear">1999</p>
+
+            </div>
+        </div>
+    )
+}
