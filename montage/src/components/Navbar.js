9c4645f3546265cd09019bd3a88d2e4348c9c982
Radiant <ronniezhu2017@gmail.com>
2022-06-10 22:30:27 -0700

    initialize my branch

--- montage/src/components/Navbar.js
+++ montage/src/components/Navbar.js
@@ -3,13 +3,13 @@ you back to the main page), the menu, the search bar, and user login option.*/
 
 export default function Navbar() {
     return (
-        <div class = "navbar">
+        <div className = "navbar">
             <button id = "MainButton" >MainButton</button>
             {/* this will later be replaced by an image of the logo */}
             <button id = "MoviesButton">MoviesButton</button>
-            <div class = "SearchContainer">
-                <form class = "SearchForm">
-                    <input type = "text" class = "SearchText" placeholder = "Search"></input>
+            <div className = "SearchContainer">
+                <form className = "SearchForm">
+                    <input type = "text" className = "SearchText" placeholder = "Search"></input>
                     <button id = "SearchButton">Q</button>
                 </form>
             </div>
