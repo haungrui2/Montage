fdf413c620d4fd499a0a4a0cb42ca8cdba450610
haungrui2 <74281429+haungrui2@users.noreply.github.com>
2022-06-11 00:38:30 -0700

    login page view

--- montage/src/App.js
+++ montage/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import MovieInfo from "./components/MovieInfo";
+import {Login} from "./components/Login/Login";
+import {Register} from "./components/Login/Register";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <MovieInfo />
+      <Login />
     </div>
   );
 }
