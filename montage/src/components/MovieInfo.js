06d4e63e80179e54b6b37106d0643fa1efca558b
Radiant <ronniezhu2017@gmail.com>
2022-06-24 00:11:36 -0700

    progress updated

--- montage/src/components/MovieInfo.js
+++ montage/src/components/MovieInfo.js
@@ -1,6 +1,7 @@
 /* This is the page for movie information, including movie name, the poster,
 rating, director, writer, genre, and description */
 import {useSelector} from 'react-redux';
+import "./style/movieInfo.css"
 
 export default function MovieInfo() {
     const visibility = useSelector(state => state.others.navbar);
